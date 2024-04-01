a40c3595aa0ed25d77943df69f5bd189fa4503fb
ismai143 <mateen.ismail@mail.utoronto.ca>
2024-04-01 15:41:50 -0400

    Part A) 1 Impute by user and item stuff

--- part_a/knn.py
+++ part_a/knn.py
@@ -1,3 +1,4 @@
+import numpy as np
 from sklearn.impute import KNNImputer
 from utils import *
 
@@ -35,9 +36,12 @@ def knn_impute_by_item(matrix, valid_data, k):
     """
     #####################################################################
     # TODO:                                                             #
-    # Implement the function as described in the docstring.             #
+    # Implement the function as described in the docstring.
+    nbrs = KNNImputer(n_neighbors=k)
+    mat = nbrs.fit_transform(np.array(matrix).transpose())
+    acc = sparse_matrix_evaluate(valid_data, np.array(mat).transpose())
+    print("Validation Accuracy: {}".format(acc))
     #####################################################################
-    acc = None
     #####################################################################
     #                       END OF YOUR CODE                            #
     #####################################################################
@@ -59,6 +63,40 @@ def main():
     # Compute the validation accuracy for each k. Then pick k* with     #
     # the best performance and report the test accuracy with the        #
     # chosen k*.                                                        #
+    K = [1, 6, 11, 16, 21, 26]
+    print("part a) Impute by user")
+    max_acc = []
+    max_k = 1
+    print("\n")
+    for k in K:
+        print(f"for k = {k}")
+        acc = knn_impute_by_user(sparse_matrix, val_data, k)
+        max_acc.append(acc)
+        if acc == max(max_acc):
+            max_k = k
+        print("")
+
+    print(f"best k was {max_k}, so we select it")
+
+    print("This achieves a test accuracy of:")
+    knn_impute_by_user(sparse_matrix, test_data, max_k)
+
+    print("part c) Impute by item")
+    max_acc = []
+    max_k = 1
+    print("\n")
+    for k in K:
+        print(f"for k = {k}")
+        acc = knn_impute_by_item(sparse_matrix, val_data, k)
+        max_acc.append(acc)
+        if acc == max(max_acc):
+            max_k = k
+        print("")
+
+    print(f"best k was {max_k}, so we select it")
+
+    print("This achieves a test accuracy of:")
+    knn_impute_by_item(sparse_matrix, test_data, max_k)
     #####################################################################
     pass
     #####################################################################
